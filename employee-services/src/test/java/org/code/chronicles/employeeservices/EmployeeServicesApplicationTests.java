d96ed579723a29b7b669775124121a39f4ff0fed
hiteshshridhar <honey7sharma@gmail.com>
2024-03-03 01:36:48 +0100

    added EmployeeServices, Department services and Maven dependencies to project.

---
+++ employee-services/src/test/java/org/code/chronicles/employeeservices/EmployeeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code.chronicles.employeeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
